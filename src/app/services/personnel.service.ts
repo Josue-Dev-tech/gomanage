54028083208e5800ebbf21834e514ecbf4370037
unknown <agbotonjosue3@gmail.com>
2022-03-04 00:51:17 +0100

    is mentor

--- src/app/services/personnel.service.ts
+++ src/app/services/personnel.service.ts
@@ -11,7 +11,7 @@ export class PersonnelService
       id: 1,
       name: 'Josue AGBOTON',
       profil: 'https://media.glamour.com/photos/5696762a16d0dc3747f0617c/master/w_1600%2Cc_limit/sex-love-life-2012-03-0217-jake-doctor_at.jpg',
-      age: 23,
+      age: 39,
       prime: 0.4344,
       salary: 3204609,
       registrationDate: new Date(),
@@ -40,7 +40,7 @@ export class PersonnelService
       id: 4,
       name: 'Christope LOPO',
       profil: 'https://media.glamour.com/photos/5696762a16d0dc3747f0617c/master/w_1600%2Cc_limit/sex-love-life-2012-03-0217-jake-doctor_at.jpg',
-      age: 33,
+      age: 36,
       prime: 0.4344,
       salary: 3204609,
       registrationDate: new Date(),
@@ -54,6 +54,26 @@ export class PersonnelService
       prime: 0.4344,
       salary: 3204609,
       registrationDate: new Date(),
+    },
+    {
+      id: 4,
+      name: 'Christope LOPO',
+      profil: 'https://media.glamour.com/photos/5696762a16d0dc3747f0617c/master/w_1600%2Cc_limit/sex-love-life-2012-03-0217-jake-doctor_at.jpg',
+      age: 26,
+      prime: 0.4344,
+      salary: 3204609,
+      registrationDate: new Date(),
+      adresse: 'Porto'
+    },
+    {
+      id: 4,
+      name: 'Christope NIEOL',
+      profil: 'https://media.glamour.com/photos/5696762a16d0dc3747f0617c/master/w_1600%2Cc_limit/sex-love-life-2012-03-0217-jake-doctor_at.jpg',
+      age: 37,
+      prime: 0.4344,
+      salary: 3204609,
+      registrationDate: new Date(),
+      adresse: 'Porto'
     },
 
   ];
